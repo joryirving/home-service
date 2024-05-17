ce8842f50199dfa929f007d1002432d5a6ce5acf
Jory Irving <jory@jory.dev>
2024-05-17 15:07:59 +0000

    chore: smol config changes

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-echo "blocky external query"
+echo "BLOCKY external query"
 dig +short @192.168.1.122 google.com | sed 's/^/  /'
-echo "bind external query"
+echo "BIND external query"
 dig +short @192.168.1.121 google.com | sed 's/^/  /'
-echo "-----------------------------"
-echo "blocky internal query"
-dig +short @192.168.1.122 nas.jory.dev | sed 's/^/  /'
-echo "bind internal query"
-dig +short @192.168.1.121 nas.jory.dev | sed 's/^/  /'
-echo "-----------------------------"
-echo "blocky cluster query"
+echo "---"
+echo "BLOCKY internal query"
+dig +short @192.168.1.122 unifi.internal | sed 's/^/  /'
+echo "BIND internal query"
+dig +short @192.168.1.121 unifi.jory.dev | sed 's/^/  /'
+echo "---"
+echo "BLOCKY main cluster query"
 dig +short @192.168.1.122 echo-server.jory.dev | sed 's/^/  /'
-echo "bind cluster query"
+echo "BIND main cluster query"
 dig +short @192.168.1.121 echo-server.jory.dev | sed 's/^/  /'
-echo "-----------------------------"
+echo "---"
+echo "BLOCKY reverse query"
+dig +short @192.168.1.122 -x 10.69.1.100 | sed 's/^/  /'
+echo "---"
