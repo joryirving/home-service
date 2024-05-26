f657ca0c1b2564b9dbab95ba7f2cea1024bfaf1a
Jory Irving <jory@jory.dev>
2024-05-26 10:55:50 -0600

    feat: update hack

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -2,18 +2,12 @@
 
 echo "BLOCKY PRIMARY external query"
 dig +short @192.168.1.2 google.com | sed 's/^/  /'
-echo "BIND PRIMARY external query"
-dig +short @192.168.1.2 -p 5301 google.com | sed 's/^/  /'
 echo "---"
 echo "BLOCKY PRIMARY internal query"
 dig +short @192.168.1.2 unifi.internal | sed 's/^/  /'
-echo "BIND PRIMARY internal query"
-dig +short @192.168.1.2 -p 5301 unifi.jory.dev | sed 's/^/  /'
 echo "---"
 echo "BLOCKY PRIMARY main cluster query"
 dig +short @192.168.1.2 echo-server.jory.dev | sed 's/^/  /'
-echo "BIND PRIMARY main cluster query"
-dig +short @192.168.1.2 -p 5301 echo-server.jory.dev | sed 's/^/  /'
 echo "---"
 echo "BLOCKY PRIMARY reverse query"
 dig +short @192.168.1.2 -x 10.69.1.100 | sed 's/^/  /'
@@ -21,19 +15,23 @@ echo "---"
 
 echo "BLOCKY SECONDARY external query"
 dig +short @192.168.1.3 google.com | sed 's/^/  /'
-echo "BIND SECONDARY external query"
-dig +short @192.168.1.3 -p 5301 google.com | sed 's/^/  /'
 echo "---"
 echo "BLOCKY SECONDARY internal query"
 dig +short @192.168.1.3 unifi.internal | sed 's/^/  /'
-echo "BIND SECONDARY internal query"
-dig +short @192.168.1.3 -p 5301 unifi.jory.dev | sed 's/^/  /'
 echo "---"
 echo "BLOCKY SECONDARY main cluster query"
 dig +short @192.168.1.3 echo-server.jory.dev | sed 's/^/  /'
-echo "BIND SECONDARY main cluster query"
-dig +short @192.168.1.3 -p 5301 echo-server.jory.dev | sed 's/^/  /'
 echo "---"
 echo "BLOCKY SECONDARY reverse query"
 dig +short @192.168.1.3 -x 10.69.1.100 | sed 's/^/  /'
+echo "---"
+
+echo "UNIFI external query"
+dig +short @192.168.1.1 google.com | sed 's/^/  /'
+echo "---"
+echo "UNIFI internal query - (this should fail)"
+dig +short @192.168.1.1 unifi.jory.dev | sed 's/^/  /'
+echo "---"
+echo "UNIFI main cluster query"
+dig +short @192.168.1.1 echo-server.jory.dev | sed 's/^/  /'
 echo "---"
